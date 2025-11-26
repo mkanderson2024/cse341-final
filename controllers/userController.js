75224009d841a1dda327de15ffb1dfa323c30007
svpontes <svpontes2015@gmail.com>
2025-11-26 20:53:42 +0000

    starting code userController assuming mongodb collection named users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,21 @@
 const mongodb = require("./config/db");
 const { objectId } = require('mongobd');
 
-const collectionName = 'users';
+const collectionName = 'users';//Here I assume the collection name will be 'users'. Change accordingly if necessary!
 
 //GET all users
-
+const getAllUsers = async (req, res) => {
+    
+    //try-catch block
+    try {
+        const db = mongodb.getDb();
+        const users = await db.collection(collectionName).find().toArray();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching all users!", error);
+        res.status(500).json({ message: "Failed to fetch all users!", error: error.message });
+    }
+}
 //GET user by ID
 
 //POST create a new user
@@ -12,4 +23,3 @@ const collectionName = 'users';
 //PUT update user
 
 //DELETE use
-
