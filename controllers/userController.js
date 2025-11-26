339176db314b9051bccdbbea86b5a5666ebc2a35
svpontes <svpontes2015@gmail.com>
2025-11-26 22:01:56 +0000

    userController CRUD starting code - need confirmation on name collection and fields names

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const mongodb = require("./config/db");
-const { objectId } = require('mongobd');
+const { ObjectId } = require('mongobd');
 
 const collectionName = 'users';//Here I assume the collection name will be 'users'. Change accordingly if necessary!
 
@@ -11,15 +11,139 @@ const getAllUsers = async (req, res) => {
         const db = mongodb.getDb();
         const users = await db.collection(collectionName).find().toArray();
         res.status(200).json(users);
-    } catch (error) {
+    } catch (error) { //handling error codes
         console.error("Error fetching all users!", error);
         res.status(500).json({ message: "Failed to fetch all users!", error: error.message });
     }
-}
+};
+
 //GET user by ID
+//handling error codes (400, 404, 500, 200)
+const getUserById = async (res, req) => {
+    
+    try {
+        const userId = req.params.userId;
+        
+        if (!ObjectId.isValid(userId)) {
+            return res.status(400).json({ message: "Invalid user ID format!" });//if different from mongodb requirements for id´s
+        }
+
+        const db = mongodb.getDb();
+        const user = await db.collection(collectionName).findOne({ _id: new ObjectId(userId) });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
+        res.status(200).json(user);//ok response
+      
+    } catch (error) {
+        console.error("Error fetching user by Id!", error);
+        res.status(500).json({ message: "Failed to fetch user by Id", error: error.message });
+    }
+};
 
 //POST create a new user
+const createUser = async (res, req) => {
+    try {
+        const user = {//assuming user will have all those fields
+            
+            type: req.body.type, //buyer or seller
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            password: req.body.password,//should be encypted later
+            createdAt: new Date(),
+            updatedAt: new Date()
+
+        };
+
+        const db = mongodb.getDb();
+        const result = await db.collection(collectionName).insertOne(user);
+
+        if (result.acknowledge) {
+            res.status(201).jason({ message: "New user created successfuly!", userId: result.insertedId });
+        } else {
+            res.status(500).json({ message: 'Failed to create a new user' });
+        }
+    } catch (error) {
+        
+        console.error('Error creating a new user:', error);
+        res.status(500).json({ message: 'Failed to create a new user', error: error.message });
+    }
+};
 
 //PUT update user
 
+const updateUser = async (res, req) => {
+    
+    try {
+        const userId = req.params.userId;
+
+        if (!ObjectId.isValid(userId)) {
+            return res.status(400).json({ message: 'Invalid user ID format' });
+        }
+
+        const updatedUser = {//assuming user will have all those fields
+        
+            type: req.body.type,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            password: req.body.password,
+            updatedAt: new Date()
+      
+        };
+
+        const db = mongodb.getDb();
+        const result = await db.collection(collectionName).replaceOne(
+            { _id: new ObjectId(userId) },
+            updatedUser
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+
+        res.status(200).json({ message: 'User updated successfully!' });
+
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ message: 'Failed to update user!', error: error.message });
+    }
+    
+};
+
 //DELETE use
+
+const deleteUser = async (res, req) => {
+    
+   try {
+    const userId = req.params.userId;
+
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: 'Invalid user ID format' });
+    }
+
+    const db = mongodb.getDb();
+    const result = await db.collection(collectionName).deleteOne({ _id: new ObjectId(userId) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ message: 'Failed to delete user', error: error.message });
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};
