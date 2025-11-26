56cb3cdd595a06cf1f99cfd727366404864b2bf0
svpontes <svpontes2015@gmail.com>
2025-11-26 20:23:24 +0000

    initial userController code

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+const mongodb = require("./config/db");
+const { objectId } = require('mongobd');
+
+const collectionName = 'users';
+
+//GET all users
+
+//GET user by ID
+
+//POST create a new user
+
+//PUT update user
+
+//DELETE use
+
