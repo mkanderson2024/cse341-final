a99f14b4f665dcfa18541a7f2bae7525a165b856
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-29 11:03:37 -0600

    swagger documentation host site update to render.com

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-    host: 'localhost:3000',
-    schemes: ['http']
+    host: 'cse341-final-56l4.onrender.com',
+    schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
