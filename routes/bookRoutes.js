4fde582766296526e61c857782a045822dd5f671
mkanderson2024 <kalani2014.ma@gmail.com>
2025-11-28 22:11:47 -0700

    Added book validation.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
+const {
+    validateBookCreation,
+    validateUpdatingBook,
+    validateDeletingBook } = require('../validators/bookValidator')
+
+const { validationResult } = require('express-validator')
+
+// Middleware to handle validation errors in results
+const handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Return 400 Bad Request with array of the error messages
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
 
 // GET all books
 router.get('/', bookController.getAllBooks);
@@ -9,12 +25,12 @@ router.get('/', bookController.getAllBooks);
 router.get('/:bookId', bookController.getBookById);
 
 // POST create new book
-router.post('/', bookController.createBook);
+router.post('/', validateBookCreation,handleValidationErrors,bookController.createBook);
 
 // PUT update book by ID
-router.put('/:bookId', bookController.updateBook);
+router.put('/:bookId', validateUpdatingBook,handleValidationErrors,bookController.updateBook);
 
 // DELETE book by ID
-router.delete('/:bookId', bookController.deleteBook);
+router.delete('/:bookId', validateDeletingBook,handleValidationErrors,bookController.deleteBook);
 
 module.exports = router;
