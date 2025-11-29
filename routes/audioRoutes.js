30b00f32545ef55cb997ceed9d28be104e9eb8fe
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-29 09:21:33 -0600

    routes for audiobooks

---
+++ routes/audioRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const audioController = require('../controllers/audioController');
+const {
+    audioBookValidationRules,
+    ValidationAudioBook
+    } = require('../validators/audioValidator')
+
+// GET all audiobooks
+router.get('/', audioController.getAllAudio);
+
+// GET single audiobook by ID
+router.get('/:audioId', audioController.getAudioById);
+
+// POST create new audiobook
+router.post('/', audioBookValidationRules, ValidationAudioBook, audioController.createAudio);
+
+// PUT update audiobook by ID
+router.put('/:audioId', audioBookValidationRules, ValidationAudioBook, audioController.updateAudio);
+
+// DELETE audiobook by ID
+router.delete('/:audioId', audioController.deleteAudio);
+
+module.exports = router;
