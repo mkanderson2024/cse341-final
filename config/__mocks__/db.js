6e96a834686f519c88f06e836724cb53b259484c
svpontes <svpontes2015@gmail.com>
2025-12-03 21:09:37 +0000

    local update

---
+++ config/__mocks__/db.js
@@ -0,0 +1,13 @@
+/*module.exports = {
+  getDb: jest.fn().mockReturnValue({
+    collection: () => ({
+      find: () => ({
+        toArray: () => Promise.resolve([
+          { _id: "001", name: "Mock User One" },
+          { _id: "002", name: "Mock User Two" }
+        ])
+      })
+    })
+  })
+};
+*/
