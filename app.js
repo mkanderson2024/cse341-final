d3cb15f4ae44b8a4fab180dc344c3ca2c3bf5ddc
mkanderson2024 <kalani2014.ma@gmail.com>
2025-12-06 09:46:37 -0700

    Setting MongoStore have .default

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const passport = require('./config/passport');
 require('dotenv').config();
 
